c256e8fe44279212caa7eb10157773b5f42f060b
Sebastian Grzelak <segrz8@gmail.com>
2020-04-08 13:41:55 +0200

    Updates

---
+++ precache-manifest.fc73e38e16cbe51938cb21f9c38708b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f864c3da0eec5b3caed27d5fbb2b01",
+    "url": "/Afterbar/index.html"
+  },
+  {
+    "revision": "4055bd901f6f264f1f75",
+    "url": "/Afterbar/static/css/main.b282a9cf.chunk.css"
+  },
+  {
+    "revision": "a304a3332a485b01ed55",
+    "url": "/Afterbar/static/js/2.149b93aa.chunk.js"
+  },
+  {
+    "revision": "2c2ad43701ce7a69d6020b3d2f486ff4",
+    "url": "/Afterbar/static/js/2.149b93aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4055bd901f6f264f1f75",
+    "url": "/Afterbar/static/js/main.469f9661.chunk.js"
+  },
+  {
+    "revision": "4205841d2a0d316f2a9c",
+    "url": "/Afterbar/static/js/runtime-main.7f38874c.js"
+  },
+  {
+    "revision": "a16e723cc65d54d286df8acb82f4f5d0",
+    "url": "/Afterbar/static/media/i4.a16e723c.jpg"
+  },
+  {
+    "revision": "9ede4596545509ce8e2dcfda2d9b4f7c",
+    "url": "/Afterbar/static/media/i5.9ede4596.jpg"
+  }
+]);
