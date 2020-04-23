1b59ecc440478d36d0861d4378660d9293d186fe
Sebastian Grzelak <segrz8@gmail.com>
2020-04-23 12:09:04 +0200

    Edit imgS

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Slider from './Components/Slider'
 import simpleParallax from 'simple-parallax-js'
 
-import logo from '../src/img/dummy/afterbarLogo.jpg'
-import img1 from '../src/img/dummy/i5.jpg'
-import img2 from '../src/img/dummy/i4.jpg'
+import logo from '../src/img/afterbarLogo.jpg'
+import img1 from '../src/img/m1.jpg'
+import img1S from '../src/img/m1S.jpg'
+import img2 from '../src/img/m2.jpg'
+import img2S from '../src/img/m2S.jpg'
 
 const cardList = [
 	{
@@ -34,6 +36,51 @@ const cardList = [
 		drinkName: 'John Collins',
 		ingredients: 'gin, sok z cytryny, syrop cukrowy, woda gazowana',
 	},
+	{
+		id: 5,
+		drinkName: 'Lalile',
+		ingredients: 'gin, sok ananas, syrop brzoskwiniowy, syrop truskawkowy',
+	},
+	{
+		id: 6,
+		drinkName: 'Żar Tropików',
+		ingredients: 'grenadyna, likier blue curacao, wódka, sok pomarańczowy',
+	},
+	{
+		id: 7,
+		drinkName: 'Tequila Sunrise',
+		ingredients: 'tequila sunrise, sok pomarańczowy, grenadyna',
+	},
+	{
+		id: 8,
+		drinkName: 'Whisky z Colą',
+		ingredients: 'whisky, cola',
+	},
+	{
+		id: 9,
+		drinkName: 'Sex on the Beach',
+		ingredients: 'wódka, likier brzoskwiniowy, sok pomarańczowy, sok żurawinowy',
+	},
+	{
+		id: 10,
+		drinkName: 'Wściekły pies',
+		ingredients: 'wódka, syrop malinowy, tabasco',
+	},
+	{
+		id: 11,
+		drinkName: 'Blue Kamikaze',
+		ingredients: 'wódka, blue curacao, sok z cytryny',
+	},
+	{
+		id: 12,
+		drinkName: 'Chmurka',
+		ingredients: 'wódka, syrop blue curacao',
+	},
+	{
+		id: 13,
+		drinkName: 'Teraz Polska',
+		ingredients: 'wódka, grenadyna, sok z cytryny',
+	},
 ]
 
 class App extends React.Component {
@@ -53,7 +100,7 @@ class App extends React.Component {
 			const itemTop = item.offsetTop
 			const itemHeight = item.offsetHeight
 			const windowHeight = window.innerHeight
-			if (scrollValue > itemTop + itemHeight - windowHeight) {
+			if (scrollValue > itemTop + itemHeight / 2 - windowHeight) {
 				item.classList.add(`Card__drink--visible`)
 			}
 		}
@@ -95,7 +142,7 @@ class App extends React.Component {
 				<div className="MainPhoto">
 					<picture>
 						<source media="(min-width: 461px)" srcSet={img1} />
-						<img src={img1} className="thumbnail" alt="..." />
+						<img src={img1S} className="thumbnail" alt="..." />
 					</picture>
 				</div>
 				<div className="ButtonsContainer">
@@ -107,17 +154,17 @@ class App extends React.Component {
 				</div>
 				<section className="About txt">
 					<h1>O nas</h1>
-					<p>Afterbar to mobilny drink bar zajmujący się organizacją imprez takich jak: wesela, 18-nastki, studniówki itp. Kilkuletna przygoda za barem skłoniła nas do założenia swojej moblilnej jednostki. Praca na tym stanowisku sprawia nam ogromną radość, posiadamy tylko jeden drink bar w ofercie, jeden, którym sterują ludzie dla których bar to hobby a nie tylko możliwość zarobku. Zamawiając nas na swoją impreze, zamawiasz właścicieli strony, dla któych dobra atmosfera i zadowoleni klienci są najważniejsze. Do zobaczenia <i class="fas fa-smile lemonYellow"></i></p>
+					<p>Afterbar to mobilny drink bar zajmujący się organizacją imprez takich jak: wesela, 18-nastki, studniówki itp. Kilkuletna przygoda za barem skłoniła nas do założenia swojej moblilnej jednostki. Praca na tym stanowisku sprawia nam ogromną radość, posiadamy tylko jeden drink bar w ofercie, jeden, którym sterują ludzie dla których bar to hobby a nie tylko możliwość zarobku. Zamawiając nas na swoją impreze, zamawiasz właścicieli strony, dla któych dobra atmosfera i zadowoleni klienci są najważniejsze. Do zobaczenia <i className="fas fa-smile lemonYellow"></i></p>
 				</section>
 				<div className="photo">
 					<picture>
 						<source media="(min-width: 461px)" srcSet={img2} />
-						<img src={img2} className="thumbnail" alt="..." />
+						<img src={img2S} className="thumbnail" alt="..." />
 					</picture>
 				</div>
 				<section className="Offer txt">
 					<h1>Oferta</h1>
-					<p>W ofercie posiadamy jeden drink bar, którym dowodzimy sami. Pełen profesjonalizm i duże doświadczenie. Chcesz wiedzieć więcej? Wjedz w zakładkę <span onClick={() => this.handleScroll('Contact')}><i class="fas fa-hand-point-right lemonYellow"></i><i>kontakt</i></span> i zadzwoń do nas! </p>
+					<p>W ofercie posiadamy jeden drink bar, którym dowodzimy sami. Pełen profesjonalizm i duże doświadczenie. Chcesz wiedzieć więcej? Wejdź w zakładkę <span onClick={() => this.handleScroll('Contact')}><i className="fas fa-hand-point-right lemonYellow"></i><i>kontakt</i></span> i zadzwoń do nas! </p>
 				</section>
 				<section className="Card">
 					{card}
@@ -126,8 +173,9 @@ class App extends React.Component {
 					<Slider />
 				</section>
 				<section className="Contact txt">
+					<h1>Kontakt:</h1>
 					<p>Tel. 786 979 780</p>
-					<p><a target="_blank" rel="noopener noreferrer" href="mailto: Afterbar.pl@gmail.com">e-mail: Afterbar.pl@gmail.com</a></p>
+					<p><a target="_blank" rel="noopener noreferrer" href="mailto: Afterbar.pl@gmail.com">e-mail: afterbar.pl@gmail.com</a></p>
 				</section>
 				<footer className="Footer">
 					<p>Copyright © 2020 Afterbar - wszelkie prawa zastrzeżone</p>
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -3,15 +3,27 @@ import './Slider.scss';
 import Carousel from 'react-bootstrap/Carousel';
 
 import img1 from '../img/1.jpg';
-import img1s from '../img/1.jpg';
+import img1s from '../img/1S.jpg';
 import img2 from '../img/2.jpg';
-import img2s from '../img/2.jpg';
+import img2s from '../img/2S.jpg';
+import img3 from '../img/3.jpg';
+import img3s from '../img/3S.jpg';
+import img4 from '../img/4.jpg';
+import img4s from '../img/4S.jpg';
+import img5 from '../img/5.jpg';
+import img5s from '../img/5S.jpg';
+import img6 from '../img/6.jpg';
+import img6s from '../img/6S.jpg';
 
 const Slider = () => {
 
     const imgSet1 = [
         { path: img1, pathS: img1s, },
         { path: img2, pathS: img2s, },
+        { path: img3, pathS: img3s, },
+        { path: img4, pathS: img4s, },
+        { path: img5, pathS: img5s, },
+        { path: img6, pathS: img6s, },
     ]
 
     const carouselItems1 = imgSet1.map(item => {
